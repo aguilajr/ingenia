66e275766f731b64657168d44e6ccac8c2401add
Juan Aguilar <jr.aguilar.gonzalez@gmail.com>
2021-08-02 00:46:34 +0200

    Defines moved

--- Ex2/main.c
+++ Ex2/main.c
@@ -1,35 +1,6 @@
 #include <stdint.h>
-#include <stdbool.h>
-#include "robotAPI.h"
-#include "robot.h"
+#include "ICM20602.h"
 
-//Addresses for High and Low data registers
-#define REG_ACCEL_XOUT_H 0x3B
-#define REG_ACCEL_XOUT_L 0x3C
-#define REG_ACCEL_YOUT_H 0x3D
-#define REG_ACCEL_YOUT_L 0x3E
-#define REG_ACCEL_ZOUT_H 0x3F
-#define REG_ACCEL_ZOUT_L 0x40
-#define REG_GYRO_XOUT_H 0x43
-#define REG_GYRO_XOUT_L 0x44
-#define REG_GYRO_YOUT_H 0x45
-#define REG_GYRO_YOUT_L 0x46
-#define REG_GYRO_ZOUT_H 0x47
-#define REG_GYRO_ZOUT_L 0x48
-
-#define ICM20602_ADDRESS 0x0068
-
-typedef struct{
-volatile uint16_t axisX;
-volatile uint16_t axisY;
-volatile uint16_t axisZ;
-}axisAccel;
-
-typedef struct{
-volatile uint16_t axisX;
-volatile uint16_t axisY;
-volatile uint16_t axisZ;
-}axisGyro;
 
 void getAccelerometerValues(axisAccel *accel) {
     uint8_t accelDataLow[3] = {0};
