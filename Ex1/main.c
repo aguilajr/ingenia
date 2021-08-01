f6fd61194dcdbab7e6a55796a58f54f44923ca7b
Juan Aguilar <jr.aguilar.gonzalez@gmail.com>
2021-08-01 20:20:10 +0200

    Main robot system

---
+++ Ex1/main.c
@@ -0,0 +1,61 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "robot.h"
+
+#define SENSOR_THRESHOLD 128
+
+bool objectDetected = false;
+
+int main(void) {
+    uint8_t command = 0;
+
+    inicialization();
+
+    while(!objectDetected)
+    {
+        command = getEepromValue();
+        RobotTask(command);
+    }
+
+    RobotStop();
+
+    return 0;
+}
+
+void inicialization() {
+    SPIInit();
+    TimerInit();
+}
+
+uint8_t getEepromValue() {
+    uint8_t command = 0;
+
+    SPISensorDisable(); //Assure there are no conflicts because of ISR
+    SPIEepromEnable();
+
+    uint8_t command = SPIRead();
+
+    return command;
+}
+
+uint8_t getSensorValue() {
+    uint8_t sensorValue = 0;
+
+    SPIEepromDisable(); //Assure there are no conflicts because of ISR
+    SPISensorEnable();
+
+    sensorValue = SPIRead();
+
+    return sensorValue;
+}
+
+void TimerISR(void) {
+    uint8_t sensorValue = getSensorValue();
+
+    if(sensorValue > SENSOR_THRESHOLD)
+    {
+        RobotStop();
+        objectDetected = true;
+    }
+
+}
